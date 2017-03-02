b182578a3333142f5e1a53499d381a390b215a1c
Jing Qin <s3442172@student.rmit.edu.au>
2017-03-02 23:22:25 +1100

    - javascirpt for

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 
 	function changeHeader() {
 		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		header.classList.toggle("header-background", scrollTop >= 50 || document.body.classList.contains("nav-open"));
+		if(document.location.pathname == "/"){
+			header.classList.toggle("header-background", scrollTop >= 50 || document.body.classList.contains("nav-open"));
+		}
 	}
 
 	var didScroll = false;
